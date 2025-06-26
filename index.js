cdbfaf570920ac553cdbbc1829506d7cc3b90cd8
Daniel Silva <danielsmartins19@gmail.com>
2025-06-26 10:37:20 -0300

    eslint and validators config

--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.use(express.json())
 
 
 morgan.token('body', (req) => {
-  return req.method === 'POST' ? JSON.stringify(req.body) : ''
+    return req.method === 'POST' ? JSON.stringify(req.body) : ''
 })
 
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+    morgan(':method :url :status :res[content-length] - :response-time ms :body')
 )
 
 
@@ -26,108 +26,108 @@ app.use(
 
 //GET: retorna todas as pessoas
 app.get('/api/persons', (request, response, next) => {
-  Person.find({})
-  .then(persons => response.json(persons))
-  .catch(error => next(error))
+    Person.find({})
+        .then(persons => response.json(persons))
+        .catch(error => next(error))
 })
 
 //GET: retorna quantas pessoas tem no banco de dados
-app.get('/info', (request, response) => {
-     Person.countDocuments({})
-    .then(count => {
-      const time = new Date()
-      response.send(`
+app.get('/info', (request, response, next) => {
+    Person.countDocuments({})
+        .then(count => {
+            const time = new Date()
+            response.send(`
         <p>Phonebook has info for ${count} people</p>
         <p>${time}</p>
       `)
-    })
-    .catch(error => next(error))
-  })
+        })
+        .catch(error => next(error))
+})
 
 //GET: retorna uma pessoa em específico 
 app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error)) 
+    Person.findById(request.params.id)
+        .then(person => {
+            if (person) {
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error)) 
 })
 
 //DELETE: deleta uma pessoa
 app.delete('/api/persons/:id', (request , response, next) => {
-     Person.findByIdAndDelete(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 //POST: cria uma pessoa
 app.post('/api/persons', (request, response, next) => {
-  const { name, number } = request.body
+    const { name, number } = request.body
 
-  if (!name || !number) {
-    return response.status(400).json({ error: 'content missing' })
-  }
+    if (!name || !number) {
+        return response.status(400).json({ error: 'content missing' })
+    }
 
-  const person = new Person({ name, number })
-  person.save()
-    .then(savedPerson => response.json(savedPerson))
-    .catch(error => next(error)) 
+    const person = new Person({ name, number })
+    person.save()
+        .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error)) 
     //  erro de validação
 })
 
 //PUT: Modifica o numero de uma pessoa existente
 app.put('/api/persons/:id', (request, response, next) => {
-  const { number } = request.body
+    const { number } = request.body
   
-  if (!number) {
-    return response.status(400).json({ error: 'number is missing' }) 
-  }
+    if (!number) {
+        return response.status(400).json({ error: 'number is missing' }) 
+    }
 
-  const personUpdate = {
-    number: number,
-  }
+    const personUpdate = {
+        number: number,
+    }
 
-  // A opção { new: true } faz com que o método retorne o documento MODIFICADO, e não o original.
-  // runValidators garante que as validações do schema sejam aplicadas na atualização.
-  Person.findByIdAndUpdate(request.params.id, personUpdate, { new: true, runValidators: true})
-    .then(updatedPerson => {
-      if (updatedPerson) {
-        response.json(updatedPerson)
-      } else {
-        // Se não encontrou a pessoa para atualizar
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+    // { new: true } faz com que o método retorne o documento MODIFICADO, e não o original.
+    // runValidators garante que as validações do schema sejam aplicadas na atualização.
+    Person.findByIdAndUpdate(request.params.id, personUpdate, { new: true, runValidators: true,  context: 'query'})
+        .then(updatedPerson => {
+            if (updatedPerson) {
+                response.json(updatedPerson)
+            } else {
+                // Se não encontrou a pessoa para atualizar
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 
 //responde com um error caso não tenha endpoints com a rota 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 //tratamento de erros
-const errorHandler = (error, request, response, next) => {
-  console.error(error.name, error.message)
+const errorHandler = (error, request, response) => {
+    console.error(error.name, error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).json({ error: 'id malformatado' })
-  }
+    if (error.name === 'CastError') {
+        return response.status(400).json({ error: 'id malformatado' })
+    }
 
-  if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+    if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
-  response.status(500).json({ error: 'erro interno no servidor' })
+    response.status(500).json({ error: 'erro interno no servidor' })
 }
 
 //middleware do tratamento de erros 
@@ -135,5 +135,5 @@ app.use(errorHandler)
 
 const PORT =  process.env.PORT || 3001 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
--- models/person.js
+++ models/person.js
@@ -1,29 +1,39 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
+require('dotenv').config()
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
+    .then(result => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
+    })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+    name: {
+        type: String,
+        minLength: 3,
+        maxLength: 32,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true,
+        minLength: 8,
+        maxLength: 15
+    },
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Person', personSchema)
